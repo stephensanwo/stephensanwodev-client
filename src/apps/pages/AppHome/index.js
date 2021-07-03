27074939bd3e2ef7f655c00b516f3601e73e50a1
stephen sanwo <me@stephensanwo.com>
2021-07-03 20:39:32 +0100

    App Page

--- src/apps/pages/AppHome/index.js
+++ src/apps/pages/AppHome/index.js
@@ -1,7 +1,46 @@
 import React from "react";
+import SideNavOptions from "./SideNavOptions";
+import "./style.scss";
+import FeaturedApps from "./FeaturedApps";
+import AppList from "./AppList";
+import Options from "./Options";
+import Footer from "../../../shared/components/Footer";
 
 const AppHome = () => {
-  return <div></div>;
+  return (
+    <div className="app-home-container">
+      <div className="app-home-sidenav">
+        <SideNavOptions />
+      </div>
+      <div className="app-home-content-container">
+        <div className="app-home-content">
+          <div className="app-home-content-items">
+            <h4
+              style={{
+                fontSize: "2rem",
+                fontFamily: "display-text-medium",
+                color: "#adbac7",
+              }}
+            >
+              Apps
+            </h4>
+            <FeaturedApps />
+
+            <div
+              className="section-divider-line"
+              style={{ marginTop: "4rem", marginBottom: "6rem" }}
+            >
+              <AppList />
+            </div>
+            <Footer />
+          </div>
+        </div>
+        <div className="blog-home-options">
+          <Options />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppHome;
--- src/data/apps.js
+++ src/data/apps.js
@@ -2,7 +2,21 @@ import corpenicus_image from "../assets/img/project-corpenicus-2.png";
 import atm_recon from "../assets/img/atm_recon.png";
 import tomori_mobile from "../assets/img/tomorii_mobile.png";
 import gallium_image from "../assets/img/SA-mortgageAIcredit.jpg";
-import { Code24 } from "@carbon/icons-react";
+
+import {
+  Api_116,
+  Application16,
+  DataStructured16,
+  LogoPython16,
+  MachineLearningModel16,
+  Settings16,
+  Wikis16,
+  Code24,
+  Code16,
+  LogoGithub16,
+  MachineLearning16,
+  Phone16,
+} from "@carbon/icons-react";
 
 export const featured_apps_data = [
   {
@@ -54,12 +68,12 @@ export const apps_data = [
     title: "Find and download anybook",
     desc: "From predicting user growth to revenue forecasts, Non-technical users across several functions can now leverage the powerful facebook prophet analytics",
     built_with: [
-      "Python, ",
-      "SQL, ",
-      "React, ",
-      "RQ, ",
-      "Javascript, ",
-      "Docker,",
+      "Python",
+      "SQL",
+      "React",
+      "RQ",
+      "Javascript",
+      "Docker",
       "MongoDB",
     ],
     logo: <Code24 fill="#539bf5" />,
@@ -70,12 +84,12 @@ export const apps_data = [
     title: "No-code forecasting at scale",
     desc: "From predicting user growth to revenue forecasts, Non-technical users across several functions can now leverage the powerful facebook prophet analytics",
     built_with: [
-      "Python, ",
-      "SQL, ",
-      "React, ",
-      "RQ, ",
-      "Javascript, ",
-      "Docker,",
+      "Python",
+      "SQL",
+      "React",
+      "RQ",
+      "Javascript",
+      "Docker",
       "MongoDB",
     ],
     logo: <Code24 fill="#539bf5" />,
@@ -86,12 +100,12 @@ export const apps_data = [
     title: "Daily NGN exchange rate data",
     desc: "From predicting user growth to revenue forecasts, Non-technical users across several functions can now leverage the powerful facebook prophet analytics",
     built_with: [
-      "Python, ",
-      "SQL, ",
-      "React, ",
-      "RQ, ",
-      "Javascript, ",
-      "Docker,",
+      "Python",
+      "SQL",
+      "React",
+      "RQ",
+      "Javascript",
+      "Docker",
       "MongoDB",
     ],
     logo: <Code24 fill="#539bf5" />,
@@ -102,12 +116,12 @@ export const apps_data = [
     title: "Find and download anybook",
     desc: "From predicting user growth to revenue forecasts, Non-technical users across several functions can now leverage the powerful facebook prophet analytics",
     built_with: [
-      "Python, ",
-      "SQL, ",
-      "React, ",
-      "RQ, ",
-      "Javascript, ",
-      "Docker,",
+      "Python",
+      "SQL",
+      "React",
+      "RQ",
+      "Javascript",
+      "Docker",
       "MongoDB",
     ],
     logo: <Code24 fill="#539bf5" />,
@@ -119,12 +133,12 @@ export const apps_data = [
     desc: " An enterprise solution that analyses ATM activities in real-time to improve the ATM reconciliation efficiency and actively monitor ATM risks and identify areas for performance improvement.",
     image_url: atm_recon,
     built_with: [
-      "Python, ",
-      "SQL, ",
-      "React, ",
-      "RQ, ",
-      "Javascript, ",
-      "Docker,",
+      "Python",
+      "SQL",
+      "React",
+      "RQ",
+      "Javascript",
+      "Docker",
       "MongoDB",
     ],
     logo: <Code24 fill="#539bf5" />,
@@ -135,14 +149,7 @@ export const apps_data = [
     title: "Urban Development Tracking App",
     desc: "An app that uses weekly satellite imagery and computer vision algorithms, to track urbanization, and provide accurate recommendations to govt agencies.",
     image_url: corpenicus_image,
-    built_with: [
-      "Python, ",
-      "OpenCV, ",
-      "GraphQL, ",
-      "NodeJs, ",
-      "React, ",
-      "MongoDB",
-    ],
+    built_with: ["Python", "OpenCV", "GraphQL", "NodeJs", "React", "MongoDB"],
     logo: <Code24 fill="#539bf5" />,
   },
   {
@@ -151,7 +158,7 @@ export const apps_data = [
     title: "No-code forecasting at scale",
     desc: "From predicting user growth to revenue forecasts, Non-technical users across several functions can now leverage the powerful facebook prophet analytics",
     image_url: gallium_image,
-    built_with: ["Python, ", "PostgresSQL, ", "Redis, ", "React"],
+    built_with: ["Python", "PostgresSQL", "Redis", "React"],
     logo: <Code24 fill="#539bf5" />,
   },
   {
@@ -160,7 +167,58 @@ export const apps_data = [
     title: "IOS and Android Fashion Ecommerce App",
     desc: "From predicting user growth to revenue forecasts, Non-technical users across several functions can now leverage the powerful facebook prophet analytics",
     image_url: tomori_mobile,
-    built_with: ["React Native, ", "AWS"],
+    built_with: ["React Native", "AWS"],
     logo: <Code24 fill="#539bf5" />,
   },
 ];
+
+export const app_categories_small = [
+  {
+    id: 1,
+    title: "APIs & Microservices",
+    logo: <Api_116 fill="#539bf5" />,
+  },
+  {
+    id: 2,
+    title: "Code Snippets",
+    logo: <Code16 fill="#539bf5" />,
+  },
+  {
+    id: 3,
+    title: "Mobile Software Projects",
+    logo: <Application16 Application16 fill="#539bf5" />,
+  },
+  {
+    id: 4,
+    title: "SaaS Projects",
+    logo: <Settings16 fill="#539bf5" />,
+  },
+  {
+    id: 4,
+    title: "AI & Bots Projects",
+    logo: <MachineLearning16 fill="#539bf5" />,
+  },
+  {
+    id: 5,
+    title: "In Development WIP",
+    logo: <LogoGithub16 fill="#539bf5" />,
+  },
+];
+
+export const app_filters = [
+  "Python",
+  "React",
+  "Javascript",
+  "Docker",
+  "Mongo DB",
+  "PostgresSQL",
+  "Redis",
+  "Tensorflow",
+  "FastAPI",
+  "Flask",
+  "Node Js",
+  "Express",
+  "React Native",
+];
+
+export const app_sort = ["New Release", "Most Popular", "Completed Projects"];
